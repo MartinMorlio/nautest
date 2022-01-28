43236691a92a41ae4c62055a7875dc965db8048c
Martin Morlio <tincho20012017@gmail.com>
2022-01-28 14:25:32 -0300

    estilos en proceso

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -4,10 +4,10 @@ import { img_300 } from '../../config/config.js';
 
 export const Cards = ({ id, poster, title, date }) => {
   return (
-    <div className="container" >
+    <div className="media" >
       <div className="container2">
+        <img className="poster" src={`${img_300}/${poster}` || "No se encontró imágen"} key={id} />
         <div className="title"> {title || "No se encontró título"} </div>
-        <img src={`${img_300}/${poster}` || "No se encontró imágen"} key={id} />
         <div className="date"> {date || "No se encontró fecha de estreno"} </div>
       </div>
     </div>
